2e4bbf66954135b5b8c252249a765a62c7d6c41b
mjollyjoker <myth.joker@gmail.com>
2015-06-21 01:11:51 +1000

    crime category php page added, minor bug exist

---
+++ crime_category.php
@@ -0,0 +1,190 @@
+<?php
+    include 'myLib.php';
+    $hostAddr = 'localhost:5984/';
+    $crimeAgstPersonAddr = join("", array($hostAddr,'combined/_design/keywords/_view/allKeywords_against_person?group_level=1'));
+    $dataCrimeAgstPerson = curlGetData($crimeAgstPersonAddr);
+    $crimeAgstPropertyAddr = join("", array($hostAddr,'combined/_design/keywords/_view/allKeywords_against_property?group_level=1'));
+    $dataCrimeAgstProperty = curlGetData($crimeAgstPropertyAddr);
+    $drugOffenceAddr = join("", array($hostAddr,'combined/_design/keywords/_view/allKeywords_drug_offence?group_level=1'));
+    $dataDrugOffence = curlGetData($drugOffenceAddr);
+    $otherCrimeAddr = join("", array($hostAddr,'combined/_design/keywords/_view/allKeywords_crime_other?group_level=1'));
+    $dataOtherCrime = curlGetData($otherCrimeAddr);
+
+    headBegin('Crime category');
+    importPackage_crime_category();
+    css_general();
+    headEnd();
+    navigationBar();
+    print '<div id="container" style="margin-top: 120px; width: 900px; height: 500px"></div>';
+    print '<script>
+        $(function () {
+            var dataRaw_crimeAgstPerson = '.$dataCrimeAgstPerson.';
+            var dataRaw_crimeAgstProperty = '.$dataCrimeAgstProperty.';
+            var dataRaw_drugOffence = '.$dataDrugOffence.';
+            var dataRaw_otherCrime = '.$dataOtherCrime.'; 
+            var crimeData = [{      
+                                id: "crimeAgstPerson",
+                                name: "Crime Against Person",
+                                data: []
+                            },{
+                                id: "crimeAgstProperty",
+                                name: "Crime Against Property",
+                                data: []
+                            },{
+                                id: "drugOffence",
+                                name: "Drug Offence",
+                                data: []
+                            },{
+                                id: "otherCrime",
+                                name: "Other Crime",
+                                data: []
+                            }];
+            var mainPie = [];
+            var totalTweets = 0;
+
+            parseData_crimeAgstPerson(dataRaw_crimeAgstPerson);
+            parseData_crimeAgstProperty(dataRaw_crimeAgstProperty);
+            parseData_drugOffence(dataRaw_drugOffence);
+            parseData_otherCrime(dataRaw_otherCrime);
+
+            function parseData_crimeAgstPerson(data) {
+                var subTotal = 0;
+                for(var i = 0; i < data.rows.length; i++){
+                    var sentiment = "neutral";
+                    sentiment = (data.rows[i].key[0] == -1 ? "negative" : "neutral");
+                    if (sentiment == "neutral"){
+                        sentiment = (data.rows[i].key[0] == 1 ? "positive" : "neutral");
+                    }
+                    crimeData[0].data.push([
+                        sentiment,
+                        data.rows[i].value,
+                        subTotal += data.rows[i].value
+                    ]);
+                }
+                mainPie.push({
+                    name : "Crime Against Person",
+                    y : subTotal,
+                    drilldown: "crimeAgstPerson"
+                });
+                for (var i = 0; i < crimeData[0].data.length; i ++){
+                    crimeData[0].data[i][1] = crimeData[0].data[i][1]/subTotal*100;
+                }
+                totalTweets += subTotal;
+            }
+
+            function parseData_crimeAgstProperty(data) {
+                var subTotal = 0;
+                for(var i = 0; i < data.rows.length; i++){
+                    var sentiment = "neutral";
+                    sentiment = (data.rows[i].key[0] == -1 ? "negative" : "neutral");
+                    if (sentiment == "neutral"){
+                        sentiment = (data.rows[i].key[0] == 1 ? "positive" : "neutral");
+                    }
+                    crimeData[1].data.push([
+                        sentiment,
+                        data.rows[i].value,
+                        subTotal += data.rows[i].value
+                    ]);
+                }
+                mainPie.push({
+                    name : "Crime Against Property",
+                    y : subTotal,
+                    drilldown: "crimeAgstProperty"
+                });
+                for (var i = 0; i < crimeData[1].data.length; i ++){
+                    crimeData[1].data[i][1] = crimeData[1].data[i][1]/subTotal*100;
+                }
+                totalTweets += subTotal;
+            }
+
+            function parseData_drugOffence(data) {
+                var subTotal = 0;
+                for(var i = 0; i < data.rows.length; i++){
+                    var sentiment = "neutral";
+                    sentiment = (data.rows[i].key[0] == -1 ? "negative" : "neutral");
+                    if (sentiment == "neutral"){
+                        sentiment = (data.rows[i].key[0] == 1 ? "positive" : "neutral");
+                    }
+                    crimeData[2].data.push([
+                        sentiment,
+                        data.rows[i].value,
+                        subTotal += data.rows[i].value
+                    ]);
+                }
+                mainPie.push({
+                    name : "Drug Offence",
+                    y : subTotal,
+                    drilldown: "drugOffence"
+                });
+                for (var i = 0; i < crimeData[2].data.length; i ++){
+                    crimeData[2].data[i][1] = crimeData[2].data[i][1]/subTotal*100;
+                }
+                totalTweets += subTotal;
+            }
+
+            function parseData_otherCrime(data) {
+                var subTotal = 0;
+                for(var i = 0; i < data.rows.length; i++){
+                    var sentiment = "neutral";
+                    sentiment = (data.rows[i].key[0] == -1 ? "negative" : "neutral");
+                    if (sentiment == "neutral"){
+                        sentiment = (data.rows[i].key[0] == 1 ? "positive" : "neutral");
+                    }
+                    crimeData[3].data.push([
+                        sentiment,
+                        data.rows[i].value,
+                        subTotal += data.rows[i].value
+                    ]);
+                }
+                mainPie.push({
+                    name : "Other Crime",
+                    y : subTotal,
+                    drilldown: "otherCrime"
+                });
+                for (var i = 0; i < crimeData[3].data.length; i ++){
+                    crimeData[3].data[i][1] = crimeData[3].data[i][1]/subTotal*100;
+                }
+                totalTweets += subTotal;
+            }
+
+            function drawChart(){
+                for (var i = 0; i < mainPie.length; i++){
+                    mainPie[i].y = mainPie[i].y/totalTweets*100;
+                }
+
+                // Create the chart
+                $("#container").highcharts({
+                    chart: {
+                        type: "pie"
+                    },
+                    title: {
+                        text: "Crime category distribution"
+                    },
+                    plotOptions: {
+                        series: {
+                            dataLabels: {
+                                enabled: true,
+                                format: "{point.name}: {point.y:.1f}%"
+                            }
+                        }
+                    },
+
+                    tooltip: {
+                        headerFormat: \'<span style="font-size:11px">{series.name}</span><br>\',
+                        pointFormat: \'<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>\'
+                    },
+                    series: [{
+                        name: "Brands",
+                        colorByPoint: true,
+                        data: mainPie
+                    }],
+                    drilldown: {
+                        series: crimeData
+                    }
+                });
+            }
+        });
+    </script>';
+    print '</body>';
+    print '</html>';
+?>
--- myLib.php
+++ myLib.php
@@ -9,12 +9,23 @@
     }
 
     function importPackage_home(){
-        print '  <link href="http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+        print ' <link href="http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
                 <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
                 <script src="http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
                 <script src="http://code.highcharts.com/highcharts.js"></script>
                 <script src="http://code.highcharts.com/modules/drilldown.js"></script>
                 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=visualization"></script>';
+    }
+
+    function importPackage_crime_category(){
+        print '<link href="http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+               <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+               <script src="http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+               <script src="http://code.highcharts.com/highcharts.js"></script>
+               <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=visualization"></script>
+               <script src="http://code.highcharts.com/modules/exporting.js"></script>
+               <script src="http://code.highcharts.com/modules/drilldown.js"></script>
+               <link rel="stylesheet" type="text/css" href="style_general.css"/>';
     }
 
     function css_general(){
@@ -38,12 +49,12 @@
             </div>
             <div>
               <ul class="nav navbar-nav">
-                <li><a href="general.html"><span class="image glyphicon glyphicon glyphicon-th-large"></span><span class="navcontent">General</span></a></li>
-                <li><a href="crime_category.html"><span class="image glyphicon glyphicon-list-alt"></span><span class="navcontent">Category</span></a></li>
-                <li><a href="trendline.html"><span class="image glyphicon glyphicon-stats"></span><span class="navcontent">Trendline</span></a></li>
-                <li><a href="compare.html"><span class="image glyphicon glyphicon-transfer"></span><span class="navcontent">Compare</span></a></li>
-                <li><a href="heatmap.html"><span class="image glyphicon glyphicon-fire"></span><span class="navcontent">Heatmap</span></a></li>
-                <li><a href="choropleth.html"><span class="image glyphicon glyphicon-globe"></span><span class="navcontent">Choropleth</span></a></li>
+                <li><a href="Home.php"><span class="image glyphicon glyphicon glyphicon-th-large"></span><span class="navcontent">General</span></a></li>
+                <li><a href="crime_category.php"><span class="image glyphicon glyphicon-list-alt"></span><span class="navcontent">Category</span></a></li>
+                <li><a href="trendline.php"><span class="image glyphicon glyphicon-stats"></span><span class="navcontent">Trendline</span></a></li>
+                <li><a href="compare.php"><span class="image glyphicon glyphicon-transfer"></span><span class="navcontent">Compare</span></a></li>
+                <li><a href="heatmap.php"><span class="image glyphicon glyphicon-fire"></span><span class="navcontent">Heatmap</span></a></li>
+                <li><a href="choropleth.php"><span class="image glyphicon glyphicon-globe"></span><span class="navcontent">Choropleth</span></a></li>
               </ul>
             </div>
           </div>
